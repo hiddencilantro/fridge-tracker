1fa7ada01ee83a77e55a273cabfe3211b9d20ebf
hiddencilantro <stevekim1109@gmail.com>
2021-03-05 11:23:49 -0500

    enabled sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "849b00e9e5bc8a20f38e5450b02a74d8aa141cf12b5c09aa9b13272eeb00e49c"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
