c461693cd936cb8dc154a3004eda36d3a1efc4ed
hiddencilantro <stevekim1109@gmail.com>
2021-03-05 10:59:51 -0500

    get /signup route & form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ApplicationController
-    
+
+    get '/signup' do
+        erb :'sessions/signup'
+    end
+
+    post '/signup' do
+        
+    end
+
+    get '/login' do
+        
+    end
+
+    post '/login' do
+
+    end
 end
