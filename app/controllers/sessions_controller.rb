c48cab77579989874c05d5834bed3c12b9bd0a20
hiddencilantro <stevekim1109@gmail.com>
2021-03-05 14:56:48 -0500

    added login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,39 @@
 class SessionsController < ApplicationController
 
+    get '/login' do
+        if session[:user_id]
+            redirect "/users/#{user.id}"
+        else
+            erb :'sessions/login'
+        end
+    end
+
+    post '/login' do
+        user = User.find_by_username(params[:username])
+        if user
+            if user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect "/users/#{user.id}"
+            else
+                "Incorrect password"
+            end
+        else
+            "Username does not exist"
+        end
+    end
+
     get '/signup' do
-        erb :'sessions/signup'
+        if session[:user_id]
+            "Looks like you're already logged in! Click here to return to your page or here to logout."
+        else
+            erb :'sessions/signup'
+        end
     end
 
     post '/signup' do
         user = User.create(params[:user])
         session[:user_id] = user.id
         redirect "/users/#{user.id}"
-    end
-
-    get '/login' do
-        
-    end
-
-    post '/login' do
-
     end
 
     get '/logout' do
