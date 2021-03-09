0993cfd88812e9f472af00b5ff89022e62e56985
hiddencilantro <stevekim1109@gmail.com>
2021-03-09 14:54:10 -0500

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,12 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
