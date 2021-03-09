ffd982043e52062100521b06c358f2f1b2e32bd3
hiddencilantro <stevekim1109@gmail.com>
2021-03-09 14:07:29 -0500

    rearranged controller actions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         if session[:user_id]
             redirect "/users/#{session[:user_id]}"
         else
-            erb :'sessions/login'
+            erb :'users/login'
         end
     end
 
@@ -20,20 +20,6 @@ class SessionsController < ApplicationController
         else
             "Username does not exist"
         end
-    end
-
-    get '/signup' do
-        if session[:user_id]
-            "Looks like you're already logged in! Click here to return to your page or here to logout."
-        else
-            erb :'sessions/signup'
-        end
-    end
-
-    post '/signup' do
-        user = User.create(params[:user])
-        session[:user_id] = user.id
-        redirect "/users/#{user.id}"
     end
 
     get '/logout' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if session[:user_id]
+            "Looks like you're already logged in! Click here to return to your page or here to logout."
+        else
+            erb :'users/signup'
+        end
+    end
+
+    post '/signup' do
+        user = User.create(params[:user])
+        session[:user_id] = user.id
+        redirect "/users/#{user.id}"
+    end
+
     get '/users/:id' do
         @user = User.find_by_id(params[:id])
         erb :'users/show'
