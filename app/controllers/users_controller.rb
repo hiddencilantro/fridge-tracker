763010acbe11511b06eb11199682e15e7a01b994
hiddencilantro <stevekim1109@gmail.com>
2021-03-12 15:06:46 -0500

    added change password functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,10 @@ class UsersController < ApplicationController
 
     get '/users/:id/profile' do #SHOW
         require_login
+
+        @message = session[:message]
+        session.delete(:message)
+
         erb :'users/profile'
     end
 
@@ -38,6 +42,7 @@ class UsersController < ApplicationController
     patch '/users/:id/profile' do #UPDATE
         current_user.skip_validations = true
         if current_user.update(params[:user])
+            session[:message] = "Profile successfully updated!"
             redirect "/users/#{current_user.id}/profile"
         else
             @error = "All fields required. Username must be unique."
@@ -53,6 +58,26 @@ class UsersController < ApplicationController
     delete '/users/:id' do #DESTROY
         current_user.destroy
         redirect "/"
+    end
+
+    get '/users/:id/profile/password' do
+        erb :'users/password'
+    end
+
+    patch '/users/:id/profile/password' do
+        if params[:password].empty? || params[:old_password].empty? || params[:confirm_password].empty?
+            @error = "All fields are required to change your password."
+            erb :'users/password'
+        else
+            if current_user.authenticate(params[:old_password]) && params[:password] == params[:confirm_password]
+                current_user.update(password: params[:password])
+                session[:message] = "Password successfully updated!"
+                redirect "/users/#{current_user.id}/profile"
+            else
+                @error = "Invalid entry."
+                erb :'users/password'
+            end
+        end
     end
 
 end
